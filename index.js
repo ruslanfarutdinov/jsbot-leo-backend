40c344a50797429daf4c81f3991ba368d7b21a23
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-07-06 14:39:35 -0700

    Console log the request body

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 const questions = ['Which movie won the Best Picture award in 2018?'];
 
 app.post('/question', (req, res) => {	
+	console.log(req.body);
+
 	res.status(200);
 	res.json({
 	  "fulfillmentText": questions[0],
